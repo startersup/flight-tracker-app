142ba40b0201bb47bbeef61aef12a0c40518e96b
saicharan96 <saicharan14996@gmail.com>
2021-07-17 14:30:44 +0530

    commit cors-anywhere proxy
    commit cors-anywhere proxy

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -11,24 +11,29 @@ const onFlightChange = (e) =>  {
 console.log(e.target.value);
 };
 
+const onCarrierChange = (f) => {
+    console.log(f.target.value);
+}
+
 const onChangeDate = (date, dateString) => {
     console.log(dateString);
 }
 
 const fetchData = () => {
-    fetch('https://flight-trackerapp.herokuapp.com/https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/BA/2630/arr/2021/7/17',
-    {
-        method: 'GET', 
+
+    const axios = require('axios');
+
+    axios({
+        method: 'get',
+        url: 'https://cors-anywhere.herokuapp.com/https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/BA/2630/arr/2021/7/17',
+        responseType: 'json',
         headers: {
-            appId: '4a25e0eb',
-            appKey: 'e6254fb07950dbe2ebe2d87baf740679',
+            'appId' : '4a25e0eb',
+            'appKey' : 'e6254fb07950dbe2ebe2d87baf740679'
         }
-    }
-    )
-  .then(response => response.json())
-  .then(json => 
-    {
-        console.log(json);
+      })
+        .then(function (response) {
+         console.log(response);
         });
 };
 
@@ -36,8 +41,10 @@ const fetchData = () => {
             return( <div>
                 <Navbar/>
                 <section className="d-flex searchbar-wrapper">
-                <Input placeholder="Enter Carrier Eg: EZY" size={10} onChange={onFlightChange}  className="ft-sm-inputs" />
-               <Input placeholder="Enter Flight Number" size={20} onChange={onFlightChange}  className="ft-inputs" />
+                <div>
+                <Input placeholder="Carrier Eg: EZY" size={10} onChange={onCarrierChange}  className="ft-sm-inputs" />
+                </div>
+               <Input placeholder="Flight Number Eg: 2630" size={20} onChange={onFlightChange}  className="ft-inputs" />
                <DatePicker onChange={onChangeDate}  className="ft-inputs wth-200"/>
                <Button type="primary"  className="ht-45 br-0"  onClick={fetchData}>
                   Track Status
